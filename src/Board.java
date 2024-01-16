02d1e15588309a2cf5a0574924aa2d1d856ce108
DonDuck1 <malj0042@hz.nl>
2024-01-16 12:10:43 +0100

    Board setup

--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,53 @@
 public class Board {
 
-    Spot[] spots;
+    Spot[][] spots;
 
     public Board() {
 
     }
 
+    public void initialiseBoard() {
+        // Setup white pieces
+        this.spots[0][0] = new Spot(0, 0, new Piece());
+        this.spots[1][0] = new Spot(1, 0, new Piece());
+        this.spots[2][0] = new Spot(2, 0, new Piece());
+        this.spots[3][0] = new Spot(3, 0, new Piece());
+        this.spots[4][0] = new Spot(4, 0, new Piece());
+        this.spots[5][0] = new Spot(5, 0, new Piece());
+        this.spots[6][0] = new Spot(6, 0, new Piece());
+        this.spots[7][0] = new Spot(7, 0, new Piece());
+        this.spots[0][1] = new Spot(0, 1, new Piece());
+        this.spots[1][1] = new Spot(1, 1, new Piece());
+        this.spots[2][1] = new Spot(2, 1, new Piece());
+        this.spots[3][1] = new Spot(3, 1, new Piece());
+        this.spots[4][1] = new Spot(4, 1, new Piece());
+        this.spots[5][1] = new Spot(5, 1, new Piece());
+        this.spots[6][1] = new Spot(6, 1, new Piece());
+        this.spots[7][1] = new Spot(7, 1, new Piece());
 
+        // Setup black pieces
+        this.spots[0][6] = new Spot(0, 6, new Piece());
+        this.spots[1][6] = new Spot(1, 6, new Piece());
+        this.spots[2][6] = new Spot(2, 6, new Piece());
+        this.spots[3][6] = new Spot(3, 6, new Piece());
+        this.spots[4][6] = new Spot(4, 6, new Piece());
+        this.spots[5][6] = new Spot(5, 6, new Piece());
+        this.spots[6][6] = new Spot(6, 6, new Piece());
+        this.spots[7][6] = new Spot(7, 6, new Piece());
+        this.spots[0][7] = new Spot(0, 7, new Piece());
+        this.spots[1][7] = new Spot(1, 7, new Piece());
+        this.spots[2][7] = new Spot(2, 7, new Piece());
+        this.spots[3][7] = new Spot(3, 7, new Piece());
+        this.spots[4][7] = new Spot(4, 7, new Piece());
+        this.spots[5][7] = new Spot(5, 7, new Piece());
+        this.spots[6][7] = new Spot(6, 7, new Piece());
+        this.spots[7][7] = new Spot(7, 7, new Piece());
+
+        // Other spots without pieces
+        for (int i = 2; i < 6; i++) {
+            for (int j = 0; j < 8; j++) {
+                this.spots[j][i] = new Spot(j, i, null);
+            }
+        }
+    }
 }
--- src/Spot.java
+++ src/Spot.java
@@ -1,8 +1,27 @@
 public class Spot {
-
     private Piece piece;
+    private final int x;
+    private final int y;
 
-    public Spot() {
+    public Spot(int x, int y, Piece piece) {
+        this.piece = piece;
+        this.x = x;
+        this.y = y;
+    }
 
+    public Piece getPiece() {
+        return this.piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
     }
 }
